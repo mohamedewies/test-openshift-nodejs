4922d7c746a36f50e48ce719130f824b4f8f8d1f
mohamedewies <82611490+mohamedewies@users.noreply.github.com>
2023-03-11 11:19:25 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 app.get('/', function(req, res) {
-    res.send('Hello, World!\n');
+    res.send('Hello, World from Ewies Open Shift!\n');
 });
-app.listen(8080, function() {
-    console.log('Example app listening on port 8080!');
+app.listen(3001, function() {
+    console.log('Example app listening on port 3001!');
 });
 module.exports = app;
