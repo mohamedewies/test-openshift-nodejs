2a69c8260f47416078875ea5cf361b7c330323be
mohamedewies <82611490+mohamedewies@users.noreply.github.com>
2023-03-11 12:28:47 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function(req, res) {
-    res.send('Hello, World from Ewies Open Shift!\n');
+    res.send('Hello, World from Ewies Open Shift 2.0!\n');
 });
 app.get('/mars', function(req, res) {
 res.send('Hello Mars ya Ewies!\n');
